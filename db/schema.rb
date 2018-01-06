0288ec87d7185de7f7525c086a2ce461cd37b329
masanori123 <motomuramasanori@MasasMacBook-Pro.local>
2018-01-06 15:52:25 +0900

    categoryモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180106064645) do
+ActiveRecord::Schema.define(version: 20180106064920) do
+
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "category_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "group_name", null: false
