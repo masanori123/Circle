0539cb08b06c5ffe4c0ebca59a9818025a2b2509
masanori123 <motomuramasanori@MasasMacBook-Pro.local>
2018-01-12 18:19:52 +0900

    カテゴリー先のビューへのルーティング設定
    Showのビューファイルの作成、ルーティングの設定、コントローラーで変数を定義しビューで都道府県別にユーザーが表示されるように記述

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,6 +4,11 @@ class CategoriesController < ApplicationController
     @users = User.order('id ASC').limit(20)
   end
 
+  def show
+    @category = Category.find(params[:id])
+    @users = User.all
+  end
+
   def search
     @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(20)
     respond_to do |format|
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "users#index"
   resources :users, only: [:index, :show, :edit, :update]
-  resources :categories, only: [:index] do
+  resources :categories, only: [:index, :show] do
     collection do
       get 'search'
       get 'suggest'
