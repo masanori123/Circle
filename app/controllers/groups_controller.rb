96e94b97c1bba0816911c1b3052d2bbcb1b25265
masanori123 <motomuramasanori@MasasMacBook-Pro.local>
2018-01-16 10:47:05 +0900

    Group-destroy-action
    グループを削除できるように実装、チャットの一覧のビューを作成

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,7 @@
 class GroupsController < ApplicationController
 
   def index
+    @groups = current_user.groups
   end
 
   def new
@@ -17,6 +18,9 @@ class GroupsController < ApplicationController
   end
 
   def destroy
+    group = Group.find(params[:id])
+    group.destroy
+    redirect_to root_path, notice: 'グループを削除しました'
   end
 
   private
